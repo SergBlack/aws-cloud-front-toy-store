ade0c0238d9b67e80ba2245f2f9d2c7cc52d50b2
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2021-12-12 19:36:28 +0300

    serve SPA in AWS S3 and Cloudfront services

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,10 +10,9 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Toy Store
       </Link>{' '}
       {new Date().getFullYear()}
-      {'.'}
     </Typography>
   );
 }
