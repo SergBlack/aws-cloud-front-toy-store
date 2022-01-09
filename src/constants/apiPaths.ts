c9dca93678d1f780adfe085b8c5a6abe9d6e8b91
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2022-01-09 20:58:03 +0300

    feat: integration with import file service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://m3v1qz6rx1.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://m3v1qz6rx1.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://m3v1qz6rx1.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://78a8eioo28.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://m3v1qz6rx1.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://m3v1qz6rx1.execute-api.eu-west-1.amazonaws.com/dev',
 };
